01d25971ed25a02cd070b010252a953efd725119
Антон Федяев <fearthe54@gmail.com>
2024-02-04 14:44:05 +0300

    homework-1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,58 @@
 -- SQL-команды для создания таблиц
+
+-- Table customers
+CREATE TABLE IF NOT EXISTS public.customers
+(
+    customer_id character varying COLLATE pg_catalog."default" NOT NULL,
+    company_name text COLLATE pg_catalog."default" NOT NULL,
+    contact_name text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT customers_pkey PRIMARY KEY (customer_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.customers
+    OWNER to postgres;
+
+-- Table employees
+CREATE TABLE IF NOT EXISTS public.employees
+(
+    employee_id integer NOT NULL DEFAULT nextval('employees_employee_id_seq'::regclass),
+    first_name character varying COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying COLLATE pg_catalog."default" NOT NULL,
+    title character varying COLLATE pg_catalog."default" NOT NULL,
+    birth_date date NOT NULL,
+    notes text COLLATE pg_catalog."default",
+    CONSTRAINT employees_pkey PRIMARY KEY (employee_id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.employees
+    OWNER to postgres;
+
+-- Table orders
+CREATE TABLE IF NOT EXISTS public.orders
+(
+    order_id integer NOT NULL DEFAULT nextval('orders_order_id_seq'::regclass),
+    customer_id "char" NOT NULL,
+    employee_id integer NOT NULL DEFAULT nextval('orders_employee_id_seq'::regclass),
+    order_date date NOT NULL,
+    ship_city character varying(200) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT orders_pkey PRIMARY KEY (order_id),
+    CONSTRAINT customer_fk FOREIGN KEY (customer_id)
+        REFERENCES public.customers (customer_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT employee_fk FOREIGN KEY (employee_id)
+        REFERENCES public.employees (employee_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.orders
+    OWNER to postgres;
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,64 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+import os
+
+def openCSV(path):
+    data_file = []
+    with open(path, newline='') as file:
+        reader = csv.reader(file, delimiter=',')
+        for item in reader:
+            data_file.append(item)
+        return data_file
+
+def main():
+    conn = psycopg2.connect(
+        host="localhost",
+        database="north",
+        user="postgres",
+        password="admin"
+    )
+
+    try:
+        with conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT * FROM customers")
+                rows = cur.fetchall() 
+
+                if rows == []:   
+                    file_customer_path = os.path.join('homework-1', 'north_data', 'customers_data.csv')
+
+                    customer_info = openCSV(file_customer_path)
+
+                    for customer in customer_info:
+                        if customer[0] != 'customer_id':
+                            cur.execute("INSERT INTO customers VALUES (%s, %s, %s)", (customer[0], customer[1], customer[2]))  
+                
+                cur.execute("SELECT * FROM employees")
+                rows = cur.fetchall() 
+                
+                if rows == []:  
+                    file_employees_path = os.path.join('homework-1', 'north_data', 'employees_data.csv')
+                    employeer_info = openCSV(file_employees_path)
+
+                    for employeer in employeer_info:
+                        if employeer[0] != 'employee_id':
+                            cur.execute("INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)", (employeer[0], employeer[1], employeer[2], employeer[3], employeer[4], employeer[5]))
+                            
+                cur.execute("SELECT * FROM orders")
+                rows = cur.fetchall() 
+                
+                if rows == []:  
+                    file_orders_path = os.path.join('homework-1', 'north_data', 'orders_data.csv')
+                    order_info = openCSV(file_orders_path)
+
+                    for order in order_info:
+                        if order[0] != 'order_id':
+                            cur.execute("INSERT INTO orders VALUES (%s, %s, %s, %s, %s)", (order[0], order[1], order[2], order[3], order[4]))  
+                
+    finally:
+        conn.close()
+
+
+if __name__ == '__main__':
+    main()
